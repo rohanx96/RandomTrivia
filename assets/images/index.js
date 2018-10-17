cba721e0296e14e1e2bbcbbb1fae4f3508d41794
Rohan Agarwal <rohanx96@gmail.com>
2018-10-18 00:09:55 +0530

    Add swiper view to show questions content Map answers to answer button component Handle close action on questions screen Handle navigation on quiz completion

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,2 +1,3 @@
 //@flow
 export const splash = require("./splash.jpg")
+export const cross = require("./ic_back_cross.png")
--- src/questions/QuestionsComponent.js
+++ src/questions/QuestionsComponent.js
@@ -4,13 +4,21 @@ import {
   View,
   Text,
   ImageBackground,
-  TouchableOpacity
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Image
 } from "react-native";
-import { splash } from "./../../assets/images/index";
+import { splash, cross } from "./../../assets/images/index";
+import { actionDialog } from "./../common/BottomDialogs";
+import Swiper from "react-native-swiper-animated";
+import { goBackTo, resetTo } from "./../navigation/NavigationAction";
+import HTML from "react-native-render-html";
+import Reactotron from "reactotron-react-native";
 
 export default class QuestionsComponent extends Component<{}, {}> {
   constructor(props) {
     super(props);
+    this.score = 0;
   }
 
   componentWillMount() {}
@@ -25,43 +33,125 @@ export default class QuestionsComponent extends Component<{}, {}> {
       >
         <View
           style={{
-            justifyContent: "space-around",
-            flex: 1,
-            padding: 24,
-            alignItems: "center"
+            paddingVertical: 16,
+            paddingHorizontal: 8,
+            alignItems: "center",
+            backgroundColor: "#fff",
+            flexDirection: "row",
+            justifyContent: "space-between"
           }}
         >
+          <TouchableWithoutFeedback
+            onPress={() => {
+              Reactotron.log("Quit");
+              this.props.openBottomSheet(
+                () => {
+                  return actionDialog(
+                    "Are you sure you want to quit?",
+                    undefined,
+                    () => {
+                      this.props.closeBottomSheet();
+                      resetTo("Home");
+                    },
+                    this.props.closeBottomSheet
+                  );
+                },
+                true,
+                true
+              );
+            }}
+          >
+            <Image source={cross} style={{ height: 32, width: 32 }} />
+          </TouchableWithoutFeedback>
           <Text
             style={{
-              color: "#fff",
-              fontSize: 32,
+              color: "#000",
+              fontSize: 16,
               textAlign: "center",
               fontWeight: "bold"
             }}
           >
-            {"RANDOM\nTRIVIA"}
+            {"RANDOM TRIVIA"}
           </Text>
-          <Text style={{ color: "#fff" }}>
-            {"10 Random Questions Every Time."}
-          </Text>
-          <TouchableOpacity
-            activeOpacity={0.9}
-            style={{
-              backgroundColor: "#355C95",
-              padding: 16,
-              borderRadius: 24,
-              justifyContent: "center",
-              flexDirection: "row"
-            }}
-          >
-            <Text style={{ textAlign: "center", color: "#fff", flex: 1 }}>
-              {"START"}
-            </Text>
-          </TouchableOpacity>
+          <View />
         </View>
+        <Swiper
+          ref={swiper => {
+            this.swiper = swiper;
+          }}
+          style={{
+            margin: 24,
+            backgroundColor: "#fff",
+            borderRadius: 16
+          }}
+          stack={true}
+          swiper={false}
+          backPressToBack={false}
+        >
+          {this.props.questions.map((questionData, index) => (
+            <View
+              style={{
+                padding: 16,
+                flexDirection: "column",
+                backgroundColor: "#fff",
+                flex: 1
+              }}
+            >
+              <Text>{"Question " + (index + 1)}</Text>
+              <HTML
+                html={questionData.question}
+                containerStyle={{ marginTop: 12, marginBottom: 24 }}
+                baseFontStyle={{ fontSize: 15 }}
+              />
+              {questionData.answers.map(answer => (
+                <AnswerButton
+                  text={answer.text}
+                  onPress={() => {
+                    if (answer.isCorrect) {
+                      this.score++;
+                    }
+                    if (index == this.props.questions.length - 1) {
+                      this.props.finishQuiz(this.score, 0);
+                      goBackTo("Home");
+                    } else {
+                      this.swiper.forceLeftSwipe();
+                    }
+                  }}
+                />
+              ))}
+            </View>
+          ))}
+        </Swiper>
       </ImageBackground>
     );
   }
 }
 
+class AnswerButton extends Component<{}, {}> {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <TouchableHighlight
+        style={{
+          borderColor: "#355C95",
+          borderRadius: 24,
+          borderWidth: 4,
+          padding: 12,
+          flexDirection: "row",
+          margin: 4
+        }}
+        underlayColor={"#355C9555"}
+        onPress={this.props.onPress}
+      >
+        <HTML
+          html={this.props.text}
+          containerStyle={{ flex: 1 }}
+          baseFontStyle={{ fontSize: 15, textAlign: "center" }}
+        />
+      </TouchableHighlight>
+    );
+  }
+}
